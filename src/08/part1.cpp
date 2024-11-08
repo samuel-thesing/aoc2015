6fc19ae22eda948b904effecb5612b702dc994e5
Samuel Thesing <thes.samue@gmail.com>
2024-11-08 17:08:48 +0100

    day 02

---
+++ src/08/part1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+#include <io_utils.h>
+#include <utils.h>
+#include <Logger.h>
+
+#define YEAR "2015"
+#define DAY "08"
+
+int solve(const std::string &input) {
+
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	Logger::init();
+	std::cout << "Advent of Code " << YEAR << " Day " << DAY << std::endl
+		<< "-------------------------------------------------------------" << std::endl;
+
+	std::string input = read_file("i1.txt");
+	auto result = solve(input);
+	std::cout << result << std::endl;
+}
