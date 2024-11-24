93ff66e8a54beb36f8195c43c0efdf3355f635e9
Samuel Thesing <thes.samue@gmail.com>
2024-11-24 14:32:54 +0100

    day 20 - factor sums

--- src/20/part1.cpp
+++ src/20/part1.cpp
@@ -1,13 +1,31 @@
 #include <utils.h>
 
 int solve(const std::string &input) {
+	int target = std::stoi(input);
+	target /= 10;
+
+	std::vector<int> sums = std::vector(target, 0);
+	for (int i = 1; i < target; i++) {
+		for (int j = i; j < target; j += i) {
+			sums[j] += i;
+		}
+	}
+
+	for (int i = 1; i < target; i++) {
+		if (sums[i] >= target) {
+			return i;
+		}
+	}
 
 	return 0;
 }
 
 int main(int argc, char** argv) {
     auto runner = Runner<int>(solve, 2015, 20);
-    runner.add_test_file("t1.txt", 0);
+    runner.add_test_string("40", 3);
+    runner.add_test_string("70", 4);
+    runner.add_test_string("60", 4);
+    runner.add_test_string("150", 8);
 
     runner.add_input_file("i1.txt");
 
---
+++ src/20/part2.cpp
@@ -0,0 +1,32 @@
+#include <utils.h>
+
+int solve(const std::string &input) {
+	int target = std::stoi(input);
+
+	std::vector<int> sums = std::vector(target, 0);
+	for (int i = 1; i < target; i++) {
+		for (int j = i; j < min(50 * i, target); j += i) {
+			sums[j] += i * 11;
+		}
+	}
+
+	for (int i = 1; i < target; i++) {
+		if (sums[i] >= target) {
+			return i;
+		}
+	}
+
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	auto runner = Runner<int>(solve, 2015, 20);
+	runner.add_test_string("40", 3);
+	runner.add_test_string("70", 4);
+	runner.add_test_string("60", 4);
+	runner.add_test_string("150", 8);
+
+	runner.add_input_file("i1.txt");
+
+	runner.run();
+}
